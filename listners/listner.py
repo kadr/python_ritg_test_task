3be2f87ab5c1a73b9a0ef2818d914f508c863b83
Kadr <mst@mstagency.ru>
2020-04-26 00:10:57 +0400

    Change socket to async call

--- listners/listner.py
+++ listners/listner.py
@@ -1,8 +1,8 @@
-import socket
-import sys
-from enum import Enum
-from datetime import datetime
+import asyncio
 import selectors
+import sys
+from datetime import datetime
+from enum import Enum
 
 selector = selectors.DefaultSelector()
 
@@ -13,52 +13,31 @@ class ChangeType(Enum):
     UPDATE = 'Изменен'
 
 
-def init_server(ip: str, port: int):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (ip, port)
-    server_socket.bind(server_address)
-    server_socket.listen(1)
+async def start_server(ip_: str, port: int):
+    server = await asyncio.start_server(
+        recv_send_message, ip_, port)
 
-    selector.register(fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connection)
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
 
-
-def accept_connection(server_socket):
-    # Wait for a connection
-    client_socket, client_address = server_socket.accept()
-    print('Connection from: %s:%s' % client_address)
-
-    selector.register(fileobj=client_socket, events=selectors.EVENT_READ, data=recv_send_message)
+    async with server:
+        await server.serve_forever()
 
 
-def recv_send_message(client_socket):
+async def recv_send_message(reader, writer):
     change_type = ChangeType
-    try:
-        request = client_socket.recv(256)
-        if request:
-            changes = request.decode().split(';')
-            print('{}: Файл {} размером {} байт, был {}'.format(
-                datetime.now().strftime('%d.%m.%Y %H:%M:%S'),
-                changes[0],
-                changes[1],
-                change_type[changes[2]].value.lower()
-            ))
-            client_socket.send('Success\n'.encode())
-        else:
-            selector.unregister(client_socket)
-            client_socket.close()
+    data = await reader.read(255)
+    changes = data.decode().split(';')
+    print('{}: Файл {} размером {} байт, был {}'.format(
+        datetime.now().strftime('%d.%m.%Y %H:%M:%S'),
+        changes[0],
+        changes[1],
+        change_type[changes[2]].value.lower()
+    ))
 
-    finally:
-        selector.unregister(client_socket)
-        client_socket.close()
-
-
-def manager():
-    while True:
-        events = selector.select()
-
-        for key, _ in events:
-            callback = key.data
-            callback(key.fileobj)
+    writer.write(data)
+    await writer.drain()
+    writer.close()
 
 
 if __name__ == '__main__':
@@ -74,5 +53,4 @@ if __name__ == '__main__':
 
     ip = args[args.index('-ip') + 1]
 
-    init_server(ip, port)
-    manager()
+    asyncio.run(start_server(ip, port))
